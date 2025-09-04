d21ccf6ac2ad19e2ade363119287e5a050d82ee3
Aproxia-dev <apro@owo.digital>
2025-09-04 16:57:29 +0200

    chore: remove bun2nix

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     systems.url = "github:nix-systems/default";
-
-    bun2nix.url = "github:baileyluTCD/bun2nix?tag=1.5.1";
-    bun2nix.inputs.nixpkgs.follows = "nixpkgs";
-    bun2nix.inputs.systems.follows = "systems";
   };
 
   # Use the cached version of bun2nix from the garnix cli
@@ -26,7 +22,6 @@
     {
       nixpkgs,
       systems,
-      bun2nix,
       ...
     }:
     let
@@ -37,21 +32,11 @@
       pkgsFor = eachSystem (system: import nixpkgs { inherit system; });
     in
     {
-      packages = eachSystem (system: {
-        # Produce a package for this template with bun2nix
-        default = pkgsFor.${system}.callPackage ./default.nix {
-          inherit (bun2nix.lib.${system}) mkBunDerivation;
-        };
-      });
-
       devShells = eachSystem (system: {
         default = pkgsFor.${system}.mkShell {
           packages = with pkgsFor.${system}; [
             bun
-
-            # Add the bun2nix binary to our devshell
 	    dart-sass
-            bun2nix.packages.${system}.default
           ];
 	  SASS_EMBEDDED_BIN_PATH = "${pkgsFor.${system}.dart-sass}/bin/sass";
 
